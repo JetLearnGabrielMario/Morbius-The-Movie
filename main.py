de02699920a4ac8eace55988919d327ebd74ce0a
JetLearnGabrielMario <galej.solano@gmail.com>
2025-04-03 19:01:20 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import pgzrun
+from random import randint
+
+
+TITLE = "Morbius: The Movie"
+
+WIDTH = 500
+HEIGHT = 500
+
+message = ""
+
+
+
+morbius = Actor('morbius')
+
+def draw():
+    #screen is another built in object
+    screen.clear()
+    screen.fill(color = (4, 0, 250))
+    #place morbius
+    morbius.draw()
+    screen.draw.text(message, center = (400, 10), fontsize = 30)
+
+
+def on_mouse_down(pos):
+    #print("Hello World!")
+    global message
+    if morbius.collidepoint(pos):
+        message = "Let's Go"
+        place_morbius()
+    else:
+        message = "No Go"
+
+
+place_morbius()
+pgzrun.go()
